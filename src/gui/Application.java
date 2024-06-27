ed7a617207734115d6bbaee88b52ddf16d6dca91
stevanbeljic <60798649+stevanbeljic@users.noreply.github.com>
2024-06-27 14:35:42 -0600

    Implemented saving and loading to JSON, added FlatLaf

--- src/gui/Application.java
+++ src/gui/Application.java
@@ -1,13 +1,18 @@
 package gui;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.formdev.flatlaf.FlatIntelliJLaf;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.io.File;
 
 public class Application {
 
@@ -17,80 +22,200 @@ public class Application {
         JButton delete;
         JButton markCompleted;
 
-        public Task (String name) {
-            taskName = name;
-            isCompleted = false;
+        public Task(String name) {
+            this.taskName = name;
+            this.isCompleted = false;
             delete = new JButton("Delete");
             markCompleted = new JButton("Mark Complete");
-            activateDeleteButton();
-            activateCompleteButton();
         }
 
-        public Task (String name, String completion){
-            taskName = name;
-            isCompleted = Boolean.parseBoolean(completion);
+        public Task(String name, String completion){
+            this.taskName = name;
+            this.isCompleted = Boolean.parseBoolean(completion);
             delete = new JButton("Delete");
             markCompleted = new JButton("Mark Complete");
-            activateDeleteButton();
-            activateCompleteButton();
-        }
-
-        private void activateCompleteButton(){
-            this.isCompleted = true;
-            this.markCompleted.setEnabled(false);
-            this.markCompleted.setVisible(false);
-        }
-
-        private void activateDeleteButton(){
-            this.delete.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("Deletion implementation");
-                }
-            });
         }
     }
 
-    JFrame frame = new JFrame("ToDo App");
+    JFrame frame;
     JPanel rootPanel;
-    private JTextField newTaskField = new JTextField();
-    private JButton newTaskButton = new JButton("Add Task");
-    private DefaultTableModel tableModel = new DefaultTableModel();
-    private JTable taskTable = new JTable();
-    private JScrollPane taskNameScrollPane = new JScrollPane();
+    private JTextField newTaskField;
+    private JButton newTaskButton;
+    private DefaultTableModel tableModel;
+    private JTable taskTable;
+    private JScrollPane taskNameScrollPane;
+    private JButton importListButton;
+    private JButton exportListButton;
+    private JLabel errorLabel;
 
     public Application() {
-        rootPanel = new JPanel(new MigLayout());
-        rootPanel.add(new JLabel("ToDo App"), "wrap");
+        FlatIntelliJLaf.setup();
 
-        newTaskField.setPreferredSize(new Dimension(120, 20));
-        rootPanel.add(newTaskField, "span 2");
-        rootPanel.add(newTaskButton, "wrap");
+        exportListButton = new JButton("Save ToDo List");
+        errorLabel = new JLabel("");
+
+        frame = new JFrame("ToDo App");
+        frame.setResizable(false);
+        JLabel titleLabel = new JLabel("Stevan's ToDo App");
+        JLabel initLabel = new JLabel("Enter task:");
+        newTaskField = new JTextField();
+        newTaskButton = new JButton("Add Task");
+        tableModel = new DefaultTableModel();
+        taskTable = new JTable();
+        taskNameScrollPane = new JScrollPane();
+        importListButton = new JButton("Import ToDo List");
+        rootPanel = new JPanel(new MigLayout());
+        rootPanel.requestFocusInWindow();
+        rootPanel.add(titleLabel, "wrap");
+
+        newTaskField.setPreferredSize(new Dimension(150, 20));
+        rootPanel.add(initLabel);
+        rootPanel.add(newTaskField, "cell 0 1 3 1");
+        rootPanel.add(newTaskButton, "cell 4 1");
+        rootPanel.add(importListButton, "cell 5 1");
+        rootPanel.add(exportListButton, "cell 6 1, wrap");
+        rootPanel.add(errorLabel, "span, wrap");
 
         tableModel.addColumn("Task");
         tableModel.addColumn("Completion");
         tableModel.addColumn("Mark Completed");
         tableModel.addColumn("Delete");
+        titleLabel.putClientProperty("FlatLaf.styleClass", "h1");
 
         taskTable.setModel(tableModel);
+        taskTable.setShowVerticalLines(false);
+        taskTable.setRowSelectionAllowed(false);
+        taskTable.setCellSelectionEnabled(false);
+        taskTable.setRowHeight(30);
+
         taskNameScrollPane.setViewportView(taskTable);
-        taskNameScrollPane.setPreferredSize(new Dimension(620, 100));
+        taskNameScrollPane.setPreferredSize(new Dimension(620, 150));
         taskNameScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants. VERTICAL_SCROLLBAR_ALWAYS);
         rootPanel.add(taskNameScrollPane,"span");
 
-        newTaskButton.addActionListener(new ActionListener() {
+        /*
+            Add a new Task to the todo list
+         */
+        Action addTask = new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String fieldText = newTaskField.getText();
                 if(fieldText.isBlank()){
                     return;
                 }
+
                 Task newTask = new Task(fieldText);
-                tableModel.addRow(new Object[]{newTask.taskName, newTask.isCompleted ? "Complete" : "Incomplete", newTask.markCompleted, new JPanel().add(newTask.delete)});
+                tableModel.addRow(new Object[]{newTask.taskName, newTask.isCompleted ? "Complete" : "Incomplete", "Mark Completed", "Delete"});
                 newTaskField.setText("");
                 newTaskField.requestFocusInWindow();
             }
+        };
+
+        newTaskField.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER){
+                    addTask.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
+                }
+            }
+
         });
+
+        newTaskButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addTask.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
+            }
+        });
+
+        /*
+            Save the todo list
+         */
+        exportListButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (tableModel.getRowCount() < 1) {
+                    errorLabel.setText("No data to export");
+                    return;
+                }
+
+                ObjectMapper mapper = new ObjectMapper();
+                ObjectNode rootNode = mapper.createObjectNode();
+                for (int i = 0; i < tableModel.getRowCount(); i++) {
+                    ObjectNode taskNode = mapper.createObjectNode();
+                    taskNode.put("taskName", (String) tableModel.getValueAt(i, 0));
+                    taskNode.put("completion", (String) tableModel.getValueAt(i, 1));
+                    rootNode.set("task_" + i, taskNode);
+                }
+
+                try {
+                    String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rootNode);
+                    System.out.println(jsonString);
+                    mapper.writerWithDefaultPrettyPrinter().writeValue(new File("todo_list.json"), rootNode);
+                    errorLabel.setText("File exported as todo_list.json");
+                } catch (Exception er) {
+                    er.printStackTrace();
+                    errorLabel.setText("Error exporting data");
+                }
+
+            }
+        });
+
+        importListButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(tableModel.getRowCount() > 0){
+                    errorLabel.setText("Import list added onto existing list");
+                }
+
+                ObjectMapper mapper = new ObjectMapper();
+                try {
+                    File file = new File("todo_list.json");
+
+                    JsonNode root = mapper.readTree(file);
+                    for(JsonNode subNode : root) {
+                        String name = subNode.get("taskName").asText();
+                        String completion = subNode.get("completion").asText();
+                        tableModel.addRow(new Object[]{name, completion, completion.equals("Completed") ? null : "Mark Completed", "Delete" });
+                    }
+                } catch (Exception er) {
+                    er.printStackTrace();
+                    errorLabel.setText("Unable to load todo_list.json file");
+                }
+            }
+        });
+
+        /*
+            Mark item in todo list as completed
+         */
+        Action markCompleted = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTable table = (JTable)e.getSource();
+                int selectedRow = Integer.valueOf(e.getActionCommand());
+                Object markComplete = table.getModel().getValueAt(selectedRow, 2);
+                ((DefaultTableModel)table.getModel()).setValueAt("Completed", selectedRow, 1);
+                ((DefaultTableModel)table.getModel()).setValueAt(null, selectedRow, 2);
+            }
+        };
+
+        /*
+            Delete a task from the todo list
+         */
+        Action delete = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTable table = (JTable)e.getSource();
+                int selectedRow = Integer.valueOf(e.getActionCommand());
+                Object markComplete = table.getModel().getValueAt(selectedRow, 2);
+                ((DefaultTableModel)table.getModel()).setValueAt("Completed", selectedRow, 1);
+                ((DefaultTableModel)table.getModel()).removeRow(selectedRow);
+            }
+        };
+
+        ButtonColumn buttonColumnComplete = new ButtonColumn(taskTable, markCompleted, 2);
+        ButtonColumn buttonColumnDelete = new ButtonColumn(taskTable, delete, 3);
 
         frame.add(rootPanel);
         frame.pack();
